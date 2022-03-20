336f020e18edd1e573b1ef0a1e941125b51ad9b8
Isaac3006 <64719458+Isaac3006@users.noreply.github.com>
2022-03-20 02:56:58 -0500

    Add files via upload

---
+++ src/RadioBox.js
@@ -0,0 +1,28 @@
+import React, {useState} from 'react'
+import {Collapse, Radio} from 'antd'
+import 'antd/dist/antd.less';
+const {Panel} = Collapse;
+    function RadioBox(props) {
+        const renderRadioBox = () => (
+            props.list && props.list.map((value) => (
+                <Radio key = {value.id} value = {`${value.id}`}>{value.name}</Radio>
+            ))
+        )
+        const [Value, setValue] = useState('0')
+        const handleChange = (event) => {
+            setValue(event.target.value)
+            props.handleFilters(event.target.value)
+        }
+        return (
+            <div>
+               <Collapse defaultActiveKey={['0']}>
+                    <Panel header = {props.title} key="1" >
+                        <Radio.Group onChange = {handleChange} value ={Value}>
+                            {renderRadioBox()} 
+                        </Radio.Group>   
+                    </Panel>
+               </Collapse> 
+            </div>)
+    }
+
+export default RadioBox
